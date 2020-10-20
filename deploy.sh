2941a5d97bd6cbb271d19c68a7d8588e1745b7b8
Benjamin S <ben.sommer.skovgaard@gmail.com>
2020-10-20 15:33:18 +0200

    first commit

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+XXXX="ca2"
+DROPLET_URL="bencat.dk"
+
+echo "##############################"
+echo "Building the frontend project"
+echo "##############################"
+npm run build
+
+echo "##############################"
+echo "Deploying Frontend project..."
+echo "##############################"
+
+scp -r ./build/* root@$DROPLET_URL:/var/www/$XXXX
+
